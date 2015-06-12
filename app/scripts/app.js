73ff9ffd2bcf0ff40e6d6439b79f842c6c3e49aa
Mischa Boldy <Mischa.boldy@gmail.com>
2015-06-12 16:54:49 +0200

    add activeUsers, style changes and refactors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,16 @@
 'use strict';
 angular
-  .module('learningDataApp', ['ngRoute', 'restangular', 'chart.js', 'ui.bootstrap', 'ui.checkbox'])
+  .module('learningDataApp', ['ngRoute', 'restangular', 'chart.js', 'ui.bootstrap', 'pretty-checkable', 'localytics.directives'])
 
   .config(function (RestangularProvider, $routeProvider, $locationProvider) {
     var authenticationToken = btoa('username:password');
 
     $routeProvider.
-      when('/', {templateUrl: 'views/pages/data.html', controller: 'MainCtrl' }).
+      when('/', {templateUrl: 'views/pages/overall.html', controller: 'MainCtrl' }).
       when('/users', {templateUrl: 'views/pages/users.html', controller: 'usersController' }).
       when('/daily', {templateUrl: 'views/pages/daily.html', controller: 'dailyController' }).
       when('/wordSearch', {templateUrl: 'views/pages/wordSearch.html', controller: 'wordSearchController' }).
+      when('/activeUsers', {templateUrl: 'views/pages/activeUsers.html', controller: 'activeUsersController' }).
       otherwise({ redirectTo: '/' });
 
       RestangularProvider.setDefaultHeaders({Authorization: 'Basic ' + authenticationToken});
